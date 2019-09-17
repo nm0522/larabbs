b171124ee864203c69c3076811887d2ac395d835
Alice Ni <niming0522@gmail.com>
2019-09-17 16:11:45 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -3,12 +3,13 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler{
 
     protected $allow_ext = ["png", "jpg", "gif", "jpeg"];
 
-    public function save($file, $folder, $file_prefix){
+    public function save($file, $folder, $file_prefix, $max_width = false){
         $folder_name = "uploads/images/$folder/" . date("Ymd", time());
 
         $upload_path = public_path() . '/' . $folder_name;
@@ -23,9 +24,24 @@ class ImageUploadHandler{
 
         $file->move($upload_path, $filename);
 
+        if($max_width && $extension != 'gif'){
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width){
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function($constraint){
+            $constraint->aspectRatio();
+            $constraint->upsize();
+
+            $image->save();
+        });
     }
 }
 
