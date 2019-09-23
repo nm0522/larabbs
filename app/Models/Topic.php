8052b28ecfcb573f415492e3dda60f286165a0ba
Alice Ni <niming0522@gmail.com>
2019-09-23 14:38:22 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,4 +13,24 @@ class Topic extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function scopeWithOrder($query, $order){
+        switch($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query){
+        return $query->orderBy('created_at', 'desc');
+    }
 }
